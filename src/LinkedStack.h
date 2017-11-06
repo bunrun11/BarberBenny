205d9f068b6c74ebc62e2a00be777f322aff8558
bunrun11 <bunnydolphins_8@yahoo.com>
2017-11-06 09:06:22 -0800

    added deleteAll function to LinkedStack

--- src/LinkedStack.h
+++ src/LinkedStack.h
@@ -8,6 +8,8 @@
 #ifndef LINKEDSTACK_H_
 #define LINKEDSTACK_H_
 
+#include <iostream>
+
 template <class T>
 struct Node{
     T& data;
@@ -24,6 +26,8 @@ class LinkedStack{
 	public:
 	LinkedStack();
 	~LinkedStack();
+
+	void deleteAll();
 
 	bool isEmpty();
     void push(T&);
@@ -43,8 +47,15 @@ LinkedStack<T>::LinkedStack(){
 
 template<class T>
 LinkedStack<T>::~LinkedStack(){
-	while(top != 0){
-		pop();
+	deleteAll();
+}
+
+//deletes all data in the stack
+template<class T>
+void LinkedStack<T>::deleteAll(){
+	while(!isEmpty()){
+		T data = pop();
+		delete &data;
 	}
 }
 
